829120143bd6e483119d792a676fdc15e76c4b2e
Americo Tomas <americo.tomas@fuseuniversal.com>
2019-02-15 14:30:18 +0000

    adding the video player into the app component

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # fe-mobile-okr-react-native-video-player
 Testing the video player library
 
+## links
+- tutorialpoint: https://www.tutorialspoint.com/react_native/react_native_environment_setup.htm
+- public video uri: https://gist.github.com/jsturgis/3b19447b304616f18657
+
 ## setup
 > cd .. && create-react-native-app reactNativeVideo
+> yarn add react-native-video
--- reactNativeVideo/App.js
+++ reactNativeVideo/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Video from 'react-native-video';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Video source={{uri: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp"}}   // Can be a URL or a local file.
+          ref={(ref) => {
+            this.player = ref
+          }}                                      // Store reference
+          onBuffer={this.onBuffer}                // Callback when remote video is buffering
+          onError={this.videoError}               // Callback when video cannot be loaded
+          style={styles.backgroundVideo} />
       </View>
     );
   }
@@ -18,4 +26,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  }
 });
